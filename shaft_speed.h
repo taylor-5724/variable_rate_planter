2a74b7652426eed35ba74d77619e17708e524abe
taylor-5724 <113221147+taylor-5724@users.noreply.github.com>
2022-12-15 14:43:09 -0600

    Implemented a variable to hold the rpm dividend

--- shaft_speed.h
+++ shaft_speed.h
@@ -3,7 +3,7 @@
 #include "hardware/gpio.h"
 
 #define PIO0_IRQ_0 7
-#define US_IN_MINUTE 60000000
+#define US_IN_MINUTE 60000000 //1M us in a second * 60 seconds in a minute
 
 uint64_t _const_rpm_divisor;
 
@@ -19,16 +19,16 @@ void shaft_speed_init(uint8_t gpio_number, uint8_t shaft_speed_precision, uint16
 
     _const_rpm_divisor = US_IN_MINUTE / counts_per_turn;
 
-    for(uint8_t i = 0; i < shaft_speed_precision; i++){
+    for(uint8_t i = 0; i < shaft_speed_precision and i < 2; i++){
         _const_rpm_divisor = _const_rpm_divisor * 10;
     }
 
     
 }
 
-uint16_t shaft_speed_get_rpm(){
+uint32_t shaft_speed_get_rpm(){
 
-    irq_set_enabled(PIO0_IRQ_0, 0); //Disable interupt while reading values from it
+    irq_set_enabled(PIO0_IRQ_0, false); //Disable interupt while reading values from it
 
     uint32_t rpm = _time_between_triggers / _times_triggered;
     uint64_t time_since_last_trigger = to_us_since_boot(get_absolute_time()) - _trigger_time;
@@ -36,10 +36,12 @@ uint16_t shaft_speed_get_rpm(){
     if(rpm < time_since_last_trigger and _rpm_read){
         rpm = time_since_last_trigger;
     }
-    _rpm_read = 1;
-    irq_set_enabled(PIO0_IRQ_0, 1); //Enable interupt
+    else {
+        _rpm_read = true;
+    }
+    irq_set_enabled(PIO0_IRQ_0, true); //Enable interupt
 
-    rpm = (60000000 / rpm) / 20;
+    rpm = _const_rpm_divisor / rpm;
 
     return rpm;
 }
@@ -48,13 +50,13 @@ void _shaft_speed_gpio_callback(uint _gpio, uint32_t _event_mask) {
     static uint64_t prev_trigger_time;
 
     if(_rpm_read){
-        _rpm_read = 0;
+        _rpm_read = false;
         _time_between_triggers = 0;
         _times_triggered = 0;
     }
 
     _trigger_time = to_us_since_boot(get_absolute_time());
     _time_between_triggers = _time_between_triggers + (_trigger_time - prev_trigger_time);
-    prev_trigger_time = _trigger_time;
     _times_triggered++;
+    prev_trigger_time = _trigger_time;
 }
