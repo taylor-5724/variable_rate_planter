331b98526925663b37f87a4855d283aa6add1062
taylor-5724 <113221147+taylor-5724@users.noreply.github.com>
2022-12-15 14:38:30 -0600

    Set core1 to run a squarewave generator
    Basically made a squarewave for the RPM reading functions to read

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 
 #include "shaft_speed.h"
 
@@ -9,22 +10,36 @@ bool debug_loop(repeating_timer_t *rt){
 
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
     "KHz > %d | RPM > %d"
-    , (loops * 4) / 1000, shaft_speed_get_rpm());
+    , loops / 250, shaft_speed_get_rpm());
 
     loops = 0;
 
     return true;
 }
 
+void core1() {
+    gpio_init(3);
+    gpio_set_dir(3, GPIO_OUT);
+    
+    //Generates a frequency for the rpm functions to read
+    while (true){
+        bool led_state = !led_state;
+        gpio_put(3, led_state);
+        sleep_us(600);
+        led_state = !led_state;
+        gpio_put(3, led_state);
+        sleep_us(600);
+
+    }
+}
+
 int main() {
 
     stdio_init_all();
 
-    gpio_init(25);
-    gpio_set_dir(25, GPIO_OUT);
-    gpio_put(25, 1);
+    multicore_launch_core1(core1);
 
-    shaft_speed_init(2, 2, 20);
+    shaft_speed_init(2, 0, 20);
 
     static repeating_timer_t timer;
     add_repeating_timer_ms(250, &debug_loop, NULL, &timer);
